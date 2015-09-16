79e4c3d99fc7a78527c30e11b1c68e5036e65d8d
edlectrico <edlectrico@gmail.com>
2015-09-16 17:40:33 +0200

    Regular expression modified. Now to 15000 max registers

--- weblog_formatter.py
+++ weblog_formatter.py
@@ -5,21 +5,28 @@ f = open('out_log.log', 'r')
 
 df = pd.DataFrame(columns = ['IP','Date','Request','Response HTTP','Response in bytes','Referer','User Agent'])
 
+counter = 0
 for line in f:
-  ip = re.search(r'\d{1,255}\.\d{1,255}\.\d{1,255}\.\d{1,255}', line).group()
-  date = line.split('[', 1)[1].split(']')[0]
-  request = line.split('"', 1)[1].split('"')[0]
-  response_http = line.split('" ', 1)[1].split(' ')[0]
-  response_bytes = line.split('" ')[1].split(' ')[1]
-  referer = line.split(' "')[2].replace('"', '')
-  user_agent = line.split(' "')[3].replace('"', '').replace(',', ' ')
+  if counter < 15000:
+    ip = re.search(r'\d{1,255}\.\d{1,255}\.\d{1,255}\.\d{1,255}', line).group()
+    # date = line.split('[', 1)[1].split(']')[0]
+    date = re.match(r"[^[]*\[([^]]*)\]", line).groups()[0]
+    request = line.split('"', 1)[1].split('"')[0]
+    response_http = line.split('" ', 1)[1].split(' ')[0]
+    response_bytes = line.split('" ')[1].split(' ')[1]
+    referer = line.split(' "')[2].replace('"', ' ')
+    # user_agent = line.split(' "')[3].replace('"', ' ').replace(',', ' ')
+    user_agent = line.split('"')[-2]
+    print user_agent
 
-  df.append({'IP':str(ip),'Date':str(date), 'Request':str(request), 'Response HTTP':str(response_http), 'Response in bytes':str(response_bytes), 'Referer':str(referer), 'User Agent':str(user_agent),}, ignore_index = True)
+    df.append({'IP':str(ip),'Date':str(date), 'Request':str(request), 'Response HTTP':str(response_http), 'Response in bytes':str(response_bytes), 'Referer':str(referer), 'User Agent':user_agent}, ignore_index = True)
 
-  df.loc[len(df)+1]=[ip, date, request, response_http, response_bytes, referer, user_agent]
+    df.loc[len(df)+1]=[ip, date, request, response_http, response_bytes, referer, user_agent]
+    
+    counter = counter + 1
 
   # print 'Inserting in df[' + str(len(df)) + ']'
 
 df.to_csv('weblogs.csv')
-print df
+# print df
 
