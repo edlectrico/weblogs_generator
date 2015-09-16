82b2a449b8a846cabdac4156871401f98fbc12b1
edlectrico <edlectrico@gmail.com>
2015-09-16 12:58:44 +0200

    Extracting the request

--- weblog_formatter.py
+++ weblog_formatter.py
@@ -6,5 +6,6 @@ f = open('out_log.log', 'r')
 for line in f:
   ip = re.search(r'\d{1,255}\.\d{1,255}\.\d{1,255}\.\d{1,255}', line).group() 
   date = line.split('[', 1)[1].split(']')[0]
-  print ip, date
+  request = line.split('"', 1)[1].split('"')[0]
+  print ip, date, request
 
