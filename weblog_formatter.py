c090434eeaa3e51464338c72f4db467a30c0ba54
edlectrico <edlectrico@gmail.com>
2015-09-16 13:22:27 +0200

    Added bytes response

--- weblog_formatter.py
+++ weblog_formatter.py
@@ -7,5 +7,8 @@ for line in f:
   ip = re.search(r'\d{1,255}\.\d{1,255}\.\d{1,255}\.\d{1,255}', line).group() 
   date = line.split('[', 1)[1].split(']')[0]
   request = line.split('"', 1)[1].split('"')[0]
-  print ip, date, request
+  response_http = line.split('" ', 1)[1].split(' ')[0]
+  response_bytes = line.split('" ')[1].split(' ')[1]
+
+  print ip, date, request, response_http, response_bytes
 
