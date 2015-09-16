da4bf888bc2c4b6ebb59957c1cd4321c21d753a3
edlectrico <edlectrico@gmail.com>
2015-09-16 12:56:47 +0200

    Extracting the date

--- weblog_formatter.py
+++ weblog_formatter.py
@@ -4,6 +4,7 @@ import re
 f = open('out_log.log', 'r')
 
 for line in f:
-  ip = re.search(r'\d{1,255}\.\d{1,255}\.\d{1,255}\.\d{1,255}', line).group()
-  print ip
+  ip = re.search(r'\d{1,255}\.\d{1,255}\.\d{1,255}\.\d{1,255}', line).group() 
+  date = line.split('[', 1)[1].split(']')[0]
+  print ip, date
 
