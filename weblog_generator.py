e0922042f1d5b381726b5f46f372eb5e1cba4ce6
edlectrico <edlectrico@gmail.com>
2015-09-17 09:34:55 +0200

    Using the weight_random function in utils. All the defined functions are now in utils.py

--- weblog_generator.py
+++ weblog_generator.py
@@ -1,10 +1,11 @@
-from datetime import timedelta, datetime
+from datetime import datetime
 import os
 import time
-import random
-from random import randint, randrange
+from random import randint, choice
 import requests
 import glob
+
+import utils as u
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 otime = datetime(2015, 9, 11, 10, 39, 40)
@@ -24,7 +25,7 @@ referers = 	['http://www.rankia.com/',
 		'https://www.twitter.com/',
 		'https://www.linkedin.com/'
 		]
-
+'''
 # Generate a randome date between two specific dates
 # Usage:
 # d1 = datetime.strptime('1/1/2008 1:30 PM', '%m/%d/%Y %I:%M %p')
@@ -35,10 +36,11 @@ def random_date(start, end):
   int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
   random_second = randrange(int_delta)
   return start + timedelta(seconds=random_second)
+'''
 
 initial_date = datetime.strptime('1/1/2015 1:00 PM', '%m/%d/%Y %I:%M %p')
 final_date = datetime.strptime('9/30/2015 6:00 PM', '%m/%d/%Y %I:%M %p')
-
+'''
 # pick a random row from file
 def random_line(file):
   line = next(file)
@@ -46,6 +48,7 @@ def random_line(file):
     if random.randrange(num + 2): continue
     line = aline
   return line
+'''
 
 # Get all resources from specified website
 page = requests.get('https://www.bbva.es/particulares/index.jsp')
@@ -68,15 +71,22 @@ f = open('out_log.log', 'w')
 
 while True:
   ip = str(randint(10,255)) + '.' + str(randint(0,255)) + '.' + str(randint(0,255)) + '.' + str(randint(0,255))
-  date = str(random_date(initial_date, final_date))
+  date = str(u.random_date(initial_date, final_date))
   date = date.replace(" ", ":").replace("-", "/").split(' ')[0]
-  resource = str(random.choice(resources))
+  resource = str(choice(resources))
   request = "GET " + resource
-  response = str(random.choice(http_responses))
-  response_bytes = str(random.randint(2000,5000))
-  referer = str(random.choice(referers))
-  user_agent = str(random.choice(random.choice(all_user_agents))).split("\n")[0]
+  # response = str(random.choice(http_responses)) # [200, 400, 403, 404, 500]
+  response = str(u.weighted_choice([	
+				(http_responses[0], 90), 
+				(http_responses[1], 10), 
+				(http_responses[2], 40), 
+				(http_responses[3], 30), 
+				(http_responses[4], 50)
+			     ]))
+
+  response_bytes = str(randint(2000,5000))
+  referer = str(choice(referers))
+  user_agent = str(choice(choice(all_user_agents))).split("\n")[0]
 
   # print ip, date, request, response, response_bytes, referer, user_agent
   f.write(ip + ' -' + ' - '  +'[' + date + ']' + ' ' + '"' + request + '"' + ' ' + response + ' ' + response_bytes + ' ' + '"' + referer + '"' + ' ' + '"' + user_agent + '"' + '\n')
-
